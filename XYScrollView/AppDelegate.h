389f898526a38fb633afb4267160a2016e37424f
etom <etom@etomdemacbook-pro.local>
2016-08-12 09:44:08 +0800

    Initial Commit

---
+++ XYScrollView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XYScrollView
+//
+//  Created by etom on 16/8/12.
+//  Copyright © 2016年 XY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XYScrollView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XYScrollView
+//
+//  Created by etom on 16/8/12.
+//  Copyright © 2016年 XY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
